c22ec129f47a82f0cde495fed482efe34ac7a0ee
Emmanuel Kant Duarte <you@example.com>
2020-12-20 20:06:24 -0300

    adding the theme types and adding theme on storybook

--- .storybook/withGlobalStyles.tsx
+++ .storybook/withGlobalStyles.tsx
@@ -1,11 +1,13 @@
 import { StoryFn } from '@storybook/addons'
+import { ThemeProvider } from 'styled-components'
+import theme from '../src/styles/theme'
 import GlobalStyles from '../src/styles/global'
 
 const withGlobalStyles = (storyFn: StoryFn) => (
-  <>
+  <ThemeProvider theme={theme}>
     <GlobalStyles />
       {storyFn()}
-  </>
+  </ThemeProvider>
 )
 
 export default withGlobalStyles;
