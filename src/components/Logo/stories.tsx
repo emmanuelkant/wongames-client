38fade0cc8598cb3db411ae7e67bfe391e1b411c
Emmanuel Kant Duarte <emmanuelkant.duarte@gmail.com>
2020-12-27 14:39:00 -0300

    adding types to logo and disable eslint rule

--- src/components/Logo/stories.tsx
+++ src/components/Logo/stories.tsx
@@ -1,9 +1,11 @@
 import { Story, Meta } from '@storybook/react/types-6-0'
 import Logo from '.'
 
+import { LogoProps } from './index'
+
 export default {
   title: 'Logo',
   component: Logo
 } as Meta
 
-export const Basic: Story = (args) => <Logo {...args} />
+export const Basic: Story<LogoProps> = (args) => <Logo {...args} />
