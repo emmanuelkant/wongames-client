6310686ea445417f225b515377f230d6372d063e
Emmanuel Kant Duarte <emmanuelkant.duarte@gmail.com>
2020-12-29 10:43:39 -0300

    add the button without icon

---
+++ src/components/Button/styles.ts
@@ -0,0 +1,33 @@
+import styled, { css, DefaultTheme } from 'styled-components'
+import { ButtonProps } from './'
+
+type WrapperProps = Pick<ButtonProps, 'size'>
+
+const wrapperModifiers = {
+  small: (theme: DefaultTheme) => css`
+    font-size: ${theme.font.sizes.xsmall};
+    height: 3rem;
+  `,
+  medium: (theme: DefaultTheme) => css`
+    font-size: ${theme.font.sizes.small};
+    padding: ${theme.spacings.xxsmall} ${theme.spacings.medium};
+    height: 4rem;
+  `,
+  large: (theme: DefaultTheme) => css`
+    font-size: ${theme.font.sizes.medium};
+    padding: ${theme.spacings.xxsmall} ${theme.spacings.large};
+    height: 5rem;
+  `
+}
+
+export const Wrapper = styled.button<WrapperProps>`
+  ${({ theme, size }) => css`
+    background: linear-gradient(180deg, #ff5f5f 0%, #f062c0 50%);
+    color: ${theme.colors.white};
+    border: 0;
+    border-radius: ${theme.border.radius};
+    padding: ${theme.spacings.xxsmall};
+
+    ${!!size && wrapperModifiers[size](theme)}
+  `}
+`
