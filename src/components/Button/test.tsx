78289ac3a1c4f56d02534a98ab01535271734aa5
Emmanuel Kant Duarte <emmanuelkant.duarte@gmail.com>
2020-12-29 11:11:49 -0300

    implementing the fullWidth button version

--- src/components/Button/test.tsx
+++ src/components/Button/test.tsx
@@ -3,15 +3,7 @@ import { renderWithTheme } from 'utils/tests/helpers'
 
 import Button from './'
 
-describe('Button', () => {
-  /*
-  it('should render the heading', () => {
-    const { container } = render(<Button />)
-
-    expect(screen.getByRole('heading', { name: /Button/i })).toBeInTheDocument()
-    expect(container.firstChild).toMatchSnapshot()
-  })
-  */
+describe('<Button />', () => {
   it('should render the medium size by default', () => {
     renderWithTheme(<Button>Buy Now</Button>)
 
@@ -38,6 +30,14 @@ describe('Button', () => {
       padding: '0.8rem 4.8rem',
       height: '5rem',
       'font-size': '1.6rem'
+    })
+  })
+
+  it('should render a fullWidth version', () => {
+    renderWithTheme(<Button fullWidth>Buy Now</Button>)
+
+    expect(screen.getByRole('button', { name: /Buy Now/i })).toHaveStyle({
+      width: '100%'
     })
   })
 })
