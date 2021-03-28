5c8e0aeeb2e7d73a13a1b0d03ca3de06d81f5aed
Emmanuel Kant Duarte <emmanuelkant.duarte@gmail.com>
2021-03-27 22:58:50 -0300

    feat: ribbon on banner

--- src/components/Banner/test.tsx
+++ src/components/Banner/test.tsx
@@ -1,28 +1,50 @@
 import { screen } from '@testing-library/react'
 import { renderWithTheme } from 'utils/tests/helpers'
 
-import Banner from './'
+import Banner from '.'
 
 const props = {
   img: 'https://source.unsplash.com/user/willianjusten/1042x580',
-  title: 'Defy Death',
-  subtitle: '<p>Play the new <strong>CrasLands</strong> season</p>',
-  buttonLabel: 'Buy Now',
+  title: 'Defy death',
+  subtitle: '<p>Play the new <strong>CrashLands</strong> season',
+  buttonLabel: 'Buy now',
   buttonLink: '/games/defy-death'
 }
 
 describe('<Banner />', () => {
-  it('should render the heading', () => {
+  it('should render correctly', () => {
     const { container } = renderWithTheme(<Banner {...props} />)
 
-    expect(screen.getByRole('img', { name: /Defy Death/i })).toBeInTheDocument()
     expect(
-      screen.getByRole('heading', { name: /Defy Death/i })
+      screen.getByRole('heading', { name: /Defy death/i })
     ).toBeInTheDocument()
+
     expect(
-      screen.getByRole('heading', { name: /Play the new CrasLands season/i })
+      screen.getByRole('heading', { name: /Play the new CrashLands season/i })
     ).toBeInTheDocument()
-    expect(screen.getByRole('link', { name: /Buy now/i })).toBeInTheDocument()
+
+    expect(screen.getByRole('img', { name: /Defy death/i })).toBeInTheDocument()
+
     expect(container.firstChild).toMatchSnapshot()
+  })
+
+  it('should render a Ribbon', () => {
+    renderWithTheme(
+      <Banner
+        {...props}
+        ribbon="My Ribbon"
+        ribbonSize="small"
+        ribbonColor="secondary"
+      />
+    )
+
+    const ribbon = screen.getByText(/My Ribbon/i)
+
+    expect(ribbon).toBeInTheDocument()
+    expect(ribbon).toHaveStyle({ backgroundColor: '#3CD3C1' })
+    expect(ribbon).toHaveStyle({
+      height: '2.6rem',
+      fontSize: '1.2rem'
+    })
   })
 })
